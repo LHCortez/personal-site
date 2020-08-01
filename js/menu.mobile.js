ac850b7a349e3c098aea5b3da84464a6561e3e64
LHCortez <luizcquintas@gmail.com>
2020-08-01 02:29:20 -0300

    Feito menu mobile

---
+++ js/menu.mobile.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    var x = document.getElementById("linksMobile");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+function hideMenu() {
+    var y = document.getElementById("linksMobile");
+    y.style.display = "none";
+}
